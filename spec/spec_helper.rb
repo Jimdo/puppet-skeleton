d2ac37413afd3f37a2906c245032ac63b48c54b2
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-03-25 20:39:54 +0100

    Use puppetlabs_spec_helper/module_spec_helper in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,1 @@
-require 'rspec-puppet'
-
-fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
-
-RSpec.configure do |config|
-  config.module_path = File.join(fixture_path, 'modules')
-  config.manifest_dir = File.join(fixture_path, 'manifests')
-end
+require 'puppetlabs_spec_helper/module_spec_helper'
