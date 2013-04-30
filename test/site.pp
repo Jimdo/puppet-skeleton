59f6e77861eb149cf7bc413bdcdcf865b0331d81
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-04-30 16:42:53 +0200

    Update Debian package index in test manifest

--- test/site.pp
+++ test/site.pp
@@ -1,3 +1,9 @@
 # This manifest is the entry point for `rake vagrant:provision`.
 # Use it to set up integration tests for this Puppet module.
+
+# Update Debian package index
+include apt::update
+exec { '/bin/true': notify => 'Exec[apt_update]' }
+
+# Test the module
 include skeleton
