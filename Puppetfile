a4e5ac48fb6ed4a37e6663c411b1335536b7dca9
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-05-03 13:18:39 +0200

    Update Debian package index before doing anything else

--- Puppetfile
+++ Puppetfile
@@ -2,4 +2,4 @@
 
 # Add module dependencies for this Puppet module here.
 
-mod "apt", :git => "git://github.com/puppetlabs/puppetlabs-apt.git"
+#mod "apt", :git => "git://github.com/puppetlabs/puppetlabs-apt.git"
--- test/site.pp
+++ test/site.pp
@@ -1,9 +1,12 @@
-# This manifest is the entry point for `rake vagrant:provision`.
+# This manifest is the entry point for `rake test:integration`.
 # Use it to set up integration tests for this Puppet module.
 
-# Update Debian package index
-include apt::update
-exec { '/bin/true': notify => 'Exec[apt_update]' }
+# Update Debian package index before doing anything else
+class apt_get_update {
+  exec { '/usr/bin/apt-get -y update': }
+}
+stage { 'pre': before => Stage['main'] }
+class { 'apt_get_update': stage => 'pre' }
 
 # Test the module
 include skeleton
