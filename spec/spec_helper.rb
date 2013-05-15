f76f7a065d5fad669caa8c71e27fba0d6644f9b1
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-05-15 10:49:59 +0200

    Give rspec-puppet the empty site.pp it requires

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,5 +4,5 @@ fixtures_path = File.expand_path('../../fixtures', __FILE__)
 
 RSpec.configure do |c|
   c.module_path  = File.join(fixtures_path, 'modules')
-  c.manifest_dir = File.join(fixtures_path, 'manifests')
+  c.manifest_dir = File.join(fixtures_path, 'manifests-rspec')
 end
