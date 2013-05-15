cd9e450958bf8909cbe180e80ca39b13f8b33692
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-05-15 10:13:11 +0200

    Change skeleton to provoke RSpec "duplicate declaration" error
    Duplicate declaration: Class[Skeleton] is already declared; cannot redeclare at
    /Users/mlafeldt/devel/jimdo/puppet-skeleton/fixtures/manifests/site.pp:3

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,7 @@
 # Install/configure something here
-class skeleton {
+class skeleton(
+  $command = '/bin/true'
+){
   # Replace this with meaningful resources
-  exec { 'sample_command': command => '/bin/true' }
+  exec { 'sample_command': command => $command }
 }
