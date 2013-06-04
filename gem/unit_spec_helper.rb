64487f9d8b1162c0a8d3280ecc4b6343200df851
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-06-04 23:19:17 +0200

    Eval FIXTURES_PATH in spec helper for unit tests

--- gem/unit_spec_helper.rb
+++ gem/unit_spec_helper.rb
@@ -1,6 +1,6 @@
 require 'rspec-puppet'
 
-fixtures_path = File.expand_path('../../fixtures', __FILE__)
+fixtures_path = ENV.fetch('FIXTURES_PATH', 'fixtures')
 
 RSpec.configure do |c|
   c.module_path  = File.join(fixtures_path, 'modules')
