b3f205188760ed04ae18ee4b0cba1c59410b230a
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2013-06-04 22:47:03 +0200

    Move spec helpers to gem/

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,1 @@
-require 'rspec-puppet'
-
-fixtures_path = File.expand_path('../../fixtures', __FILE__)
-
-RSpec.configure do |c|
-  c.module_path  = File.join(fixtures_path, 'modules')
-  c.manifest_dir = File.join(fixtures_path, 'manifests-rspec')
-end
+require File.expand_path('../../gem/unit_spec_helper', __FILE__)
